9c9da4b1662dfd169bd166e1de019dbda6733ee1
Patrick Dureza <pgdureza@gmail.com>
2018-05-21 14:03:16 +0800

    removed jquery dependence

--- public/javascripts/ag-forms.js
+++ public/javascripts/ag-forms.js
@@ -1,11 +1,18 @@
 (function(config) {
   var host = (Shopify && Shopify.shop) || window.location.host;
-  $("[data-agform]").each(function(){
-    $el = $(this);
-    var id = $el.data('agform');
+  document.querySelectorAll("[data-agform]").forEach(function(object, index){
+    var xhr = new XMLHttpRequest();
     var src = 'https://ag-forms-express.herokuapp.com/forms/html/';
-    $.get(src + host + '/' + id, function(html){
-      $el.html(html);
-    })
+    var id = object.dataset['agform'];
+    xhr.open('GET', src + host + '/' + id);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+          object.innerHTML = xhr.responseText
+        }
+        else {
+          console.error('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
   })
 }())
