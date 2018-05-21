9f3a65acb078cffa77ad1c2d3527eae6aad4fd75
Patrick Dureza <pgdureza@gmail.com>
2018-05-21 11:56:13 +0800

    updated exposed script to use IIFE

--- public/javascripts/ag-forms.js
+++ public/javascripts/ag-forms.js
@@ -1,12 +1,13 @@
-var AGForm = function(source, id, container){
-  this.load = function(){
-    if (source && id && container) {
-      var src = 'https://ag-forms-express.herokuapp.com/forms/html/';
-      $.get(src + source + '/' + id, function(html){
-        $(container).html(html);
-      })
-    } else {
-      console.warn('AG forms initialized but missing params passed')
-    }
+(function(config) {
+  var source = config.source;
+  var id = config.id;
+  var container = config.container;
+  if (source && id && container) {
+    var src = 'https://ag-forms-express.herokuapp.com/forms/html/';
+    $.get(src + source + '/' + id, function(html){
+      $(container).html(html);
+    })
+  } else {
+    console.warn('AG forms initialized but missing params passed')
   }
-}
+}())
